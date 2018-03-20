476d7eae2fef279d2610cf14765bce4102c089ec
Yunus Yığci <37595924+yunusyigci@users.noreply.github.com>
2018-03-20 23:21:45 +0300

    Dosyalar

---
+++ Visual Studio Dosyaları/DuyguAnalizi/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace DuyguAnalizi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Tweet_Cümlesi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.puan = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(601, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(310, 91);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Tweetleri Çek";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(357, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox1.Size = new System.Drawing.Size(218, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton1.Location = new System.Drawing.Point(6, 55);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(193, 21);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.Text = "Kullanıcı Tweetleri Çek";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton2.Location = new System.Drawing.Point(6, 18);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(192, 21);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Hashtag Tweetleri Çek";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(357, 55);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(219, 22);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(214, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Kullanıcı Adı Girin";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(212, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Tweet Sayısı Girin";
+            // 
+            // chart1
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea6);
+            this.chart1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            legend6.Name = "Legend1";
+            this.chart1.Legends.Add(legend6);
+            this.chart1.Location = new System.Drawing.Point(917, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            this.chart1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series6.Legend = "Legend1";
+            series6.Name = "Series3";
+            this.chart1.Series.Add(series6);
+            this.chart1.Size = new System.Drawing.Size(533, 532);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Sunken;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Tweet_Cümlesi,
+            this.puan});
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveBorder;
+            this.dataGridView1.Location = new System.Drawing.Point(14, 109);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(897, 435);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // Tweet_Cümlesi
+            // 
+            this.Tweet_Cümlesi.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Tweet_Cümlesi.HeaderText = "Tweet Cümlesi";
+            this.Tweet_Cümlesi.Name = "Tweet_Cümlesi";
+            // 
+            // puan
+            // 
+            this.puan.HeaderText = "Puanı";
+            this.puan.Name = "puan";
+            this.puan.Width = 78;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(213, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Hashtag Adı Girin";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.SkyBlue;
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(14, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(588, 91);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(917, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(434, 45);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(24, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(143, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Toplam Artı Puan :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Blue;
+            this.label5.Location = new System.Drawing.Point(173, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 17);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(246, 18);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(148, 17);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Toplam Eksi Puan :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(400, 18);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 17);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(1453, 552);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.chart1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Name = "Form1";
+            this.Text = "Twitter Duygu Analizi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tweet_Cümlesi;
+        private System.Windows.Forms.DataGridViewTextBoxColumn puan;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Visual Studio Dosyaları/DuyguAnalizi/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TweetSharp;
+using System.Data.SqlClient;
+
+namespace DuyguAnalizi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            kelimelrim();
+        }
+
+        SqlConnection baglanti;
+        SqlCommand komut;
+        DataTable dt = new DataTable();
+
+
+        public void kelimelrim()
+        {
+            baglanti = new SqlConnection("Data Source = YUNUS; Initial Catalog = kelimeler; Integrated Security = True");
+            baglanti.Open();
+            komut = new SqlCommand("Select *From kelime", baglanti);
+            SqlDataAdapter da = new SqlDataAdapter(komut);
+            da.Fill(dt);
+        }
+
+
+
+        TwitterService twitterservis = new TwitterService(
+            "SyZRe887viChj8ij77oGOp72t",
+            "xL8Z8GLB0eieUc9KjAwZCmI2OsWA3PmXeMKm6q8J2HVhj2NAjR",
+            "3891446608-Nyq0BsKgGSIikUL7M9v3XXNynHc8pdiFsVIyISj",
+            "GddnEoNUpFGXDZNIxzRWhitIlIdlk8MD81p3H6N7pG05P");
+
+
+        public int sayısı;
+        List<string> listtweet = new List<string>();
+        List<string> listtweet3 = new List<string>();
+        private void Form1_Load(object sender, EventArgs e)
+        { }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != ""&& textBox1.Text.Substring(0,1)!=" ")
+            {
+                artıpuan = 0;
+                eksipuan = 0;
+                tweetpuanı = 0;
+                dataGridView1.Rows.Clear();
+                listtweet.Clear();
+                listtweet3.Clear();
+                foreach (var series in chart1.Series)
+                {
+                    series.Points.Clear();
+                }
+                if (textBox1.Text != "")
+                {
+                    if (radioButton1.Checked == true)
+                        KullanıcıTweetleri();
+                    else
+                        HastagTweetleri();
+                    tweettemizle();
+                    MessageBox.Show("Analiz Tamamlandı.");
+                }
+            }
+            else
+                MessageBox.Show("Boş Alan Bırakmayın!");
+
+        }
+
+        public void HastagTweetleri()
+        {
+            int count = Convert.ToInt16(numericUpDown1.Value);
+            var veriler1 = twitterservis.Search(new SearchOptions { Q = "#" +textBox1.Text, Count = 1, Resulttype = TwitterSearchResultType.Recent });
+            int b = 0;
+            for (b = 0; b < count / 10 + 1; b++)
+            {
+                try
+                {
+                    veriler1 = twitterservis.Search(new SearchOptions { Q = "#" + textBox1.Text, Count = 10, Resulttype = TwitterSearchResultType.Recent, MaxId = veriler1.Statuses.Last().Id });
+                }
+                catch{}
+                try
+                {
+                    foreach (var item in veriler1.Statuses)
+                    {
+                        listtweet.Add(item.Text);
+                    }
+                    if (b != count / 10 && listtweet.Count > 0 && veriler1.Statuses.Count() > 0)
+                    {
+                        listtweet.RemoveAt(listtweet.Count - 1);
+                    }
+                }
+                catch { }
+
+                
+            }
+        }
+        public void KullanıcıTweetleri()
+        {
+            int count = Convert.ToInt16(numericUpDown1.Value);
+            var veriler2 = twitterservis.ListTweetsOnUserTimeline(new ListTweetsOnUserTimelineOptions() { Count = 1, ScreenName = textBox1.Text });//kullanıcı adı ile tweet çek           
+            int b = 0;
+            for (b = 0; b < count / 10+1; b++)
+            {
+                try
+                {
+                    veriler2 = twitterservis.ListTweetsOnUserTimeline(new ListTweetsOnUserTimelineOptions() { Count = 10, ScreenName = textBox1.Text, MaxId = veriler2.Last().Id });
+                }
+                catch
+                {
+                }
+                try
+                {
+                    foreach (var item in veriler2)
+                    {
+                        listtweet.Add(item.Text);
+                    }
+                    if (b != count / 10 && listtweet.Count > 0 && veriler2.Count() > 0)
+                    {
+                        listtweet.RemoveAt(listtweet.Count - 1);
+                    }
+                }
+                catch { }
+
+            }
+        }
+        public void tweettemizle()
+        {
+            Regex rgx = new Regex(@"[^0-9a-zA-Z ğüşıöçĞÜŞİÖÇ@]");
+            int i = 0;
+            foreach (string tweet in listtweet)
+            {
+                try
+                {
+                    string[] words = rgx.Replace(tweet, "").Split(' ');
+                    i++;
+                    foreach (string a in words)
+                    {
+                        if (a.Length < 18 && a.Length > 2 && a != "RT" && "htt" != a.Substring(0, 3) && "@" != a.Substring(0, 1))
+                        {
+                            listtweet3.Add(a.ToLower());
+                        }
+                    }
+                    puanlandır(tweet);
+                    listtweet3.Clear();
+                }
+                catch { }
+            }
+            grafik();
+            label5.Text = artıpuan.ToString();
+            label7.Text = eksipuan.ToString();
+        }
+        public void grafik()
+        {
+            chart1.Series["Series3"].Points.Add(artıpuan);
+            chart1.Series["Series3"].Points.Add(eksipuan);
+            chart1.Series["Series3"].Points[0].Color = Color.Blue;
+            chart1.Series["Series3"].Points[1].Color = Color.Red;
+            chart1.Series["Series3"].Points[0].AxisLabel = "Pozitif";
+            chart1.Series["Series3"].Points[1].AxisLabel = "Negatif";
+        }
+        int artıpuan = 0;
+        int eksipuan = 0;
+        int tweetpuanı = 0;
+        public void puanlandır(string a)
+        {
+            tweetpuanı = 0;
+            int t = 0;
+            foreach (var item in listtweet3)
+            {
+                if (item == "değil"|| item == "ama")
+                    t = 1;
+                DataRow[] rows = dt.Select("kelime ='" + item + "'");
+                if (rows.Length > 0)
+                {
+                    tweetpuanı += Convert.ToInt16(rows[0][1]);
+                    if ((Convert.ToInt16(rows[0][1]) < 0 && t == 1 && tweetpuanı < 0)||
+                        (Convert.ToInt16(rows[0][1]) > 0 && t == 1 && tweetpuanı > 0))
+                        t = 0;
+                }
+            }
+            if (t == 1)
+            {
+                tweetpuanı *= -1;
+            }
+            if (tweetpuanı < 0)
+                eksipuan += tweetpuanı;
+            else
+                artıpuan += tweetpuanı;
+            dataGridView1.Rows.Add(a, tweetpuanı);
+            
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            label3.Visible = true;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            label3.Visible = false;
+        }
+    }
+}
+
